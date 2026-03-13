0b2d393f7f593fe76bbcec5fa8fec741141de718
iampinaki1 <pinaki82499730@gmail.com>
2026-03-14 02:35:32 +0530

    deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 4000;
 
 // ALWAYS PUT CORS FIRST!
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
--- sockets/socketmanagement.js
+++ sockets/socketmanagement.js
@@ -10,7 +10,7 @@ export const setupSocket = (app) => {
 
     const io = new Server(server, {
         cors: {
-            origin: process.env.URL || "http://localhost:5173",
+            origin: process.env.FRONTEND_URL || "http://localhost:5173",
             methods: ['GET', 'POST'],
             credentials: true
         }
